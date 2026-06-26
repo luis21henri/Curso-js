1c8d6528f81f99931e349ee2d8d4b5d4e47bd3fe
Luis Henrique de Oliveira Rodrigues <luishenriqueoficial212@gmail.com>
2026-06-26 11:16:52 -0300

    Refactor routes.js for cleaner structure

--- routes.js
+++ routes.js
@@ -20,8 +20,6 @@ route.get('/contato/index', loginRequired, contatoController.index);
 route.post('/contato/register', loginRequired, contatoController.register);
 route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 route.post('/contato/edit/:id', loginRequired, contatoController.edit);
-
-// CORREÇÃO: Apontando para o método renomeado do controller
 route.get('/contato/delete/:id', loginRequired, contatoController.deleteContato);
 
 module.exports = route;

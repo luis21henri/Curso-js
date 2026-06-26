f8d0493e4d00d4f9f09acb1b82fb2b2baf40da7d
Luis Henrique de Oliveira Rodrigues <luishenriqueoficial212@gmail.com>
2026-06-26 11:03:52 -0300

    Fix delete route to use renamed controller method
    Updated route for deleting contact to point to renamed method.

--- routes.js
+++ routes.js
@@ -1,27 +1,27 @@
- const express = require('express')
- const route = express.Router();
- const homeController = require('./src/controllers/homeController')
- const loginController = require('./src/controllers/loginController')
- const contatoController = require('./src/controllers/contatoController')
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
- const { loginRequired } = require('./src/middlewares/middleware')
-  
+const { loginRequired } = require('./src/middlewares/middleware');
 
+// Rotas do home
+route.get('/', homeController.index);
 
- //Rotas do home
- route.get('/', homeController.index);
+// Rotas de Login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
-//Rotas de Login
-route.get('/login/index', loginController.index)
-route.post('/login/register', loginController.register)
-route.post('/login/login', loginController.login)
-route.get('/login/logout', loginController.logout)
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
 
-//Rotas de contato
-route.get('/contato/index', loginRequired, contatoController.index)
-route.post('/contato/register', loginRequired, contatoController.register)
-route.get('/contato/index/:id', loginRequired, contatoController.editIndex)
-route.post('/contato/edit/:id', loginRequired, contatoController.edit)
-route.get('/contato/delete/:id', loginRequired, contatoController.delete);
+// CORREÇÃO: Apontando para o método renomeado do controller
+route.get('/contato/delete/:id', loginRequired, contatoController.deleteContato);
 
- module.exports = route;
+module.exports = route;
